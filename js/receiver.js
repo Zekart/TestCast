655f3fc7623e3a1b3e3212d3718b69476fb069af
Zekart <zekart22@gmail.com>
2019-02-25 15:16:16 +0200

    Update receiver.js

--- js/receiver.js
+++ js/receiver.js
@@ -1,6 +1,3 @@
-var context = cast.framework.CastReceiverContext.getInstance();
-var playerManager = context.getPlayerManager();
-
 function makeRequest (method, url) {
   return new Promise(function (resolve, reject) {
     var xhr = new XMLHttpRequest();
@@ -30,41 +27,26 @@ playerManager.setMessageInterceptor(
     request => {
       console.log("Intercepting LOAD request")
       return new Promise((resolve, reject) => {
-
-        if(request.media.contentType == 'video/mp4') {
-          return resolve(request);
-        }
-
         // Fetch content repository by requested contentId
-        makeRequest('GET', request.media.contentId)
-          .then(function (data) {
-	          var item = data[request.media.contentId];
-	          if(!item) {
-	            // Content could not be found in repository
-	            reject();
-	          } else {
-	            // Adjusting request to make requested content playable
-	            request.media.contentId = item.stream.hls;
-	            request.media.contentType = 'application/x-mpegurl';
+        makeRequest('GET', 'https://tse-summit.firebaseio.com/content.json?orderBy=%22$key%22&equalTo=%22'+ request.media.contentId + '%22').then(function (data) {
+          var item = data[request.media.contentId];
+          if(!item) {
+            // Content could not be found in repository
+            reject();
+          } else {
+            // Adjusting request to make requested content playable
+            request.media.contentId = item.stream.hls;
+            request.media.contentType = 'application/x-mpegurl';
 
-	            // Add metadata
-	            var metadata = new cast.framework.messages.MovieMediaMetadata();
-	            metadata.metadataType = cast.framework.messages.MetadataType.MOVIE;
-	            metadata.title = item.title;
-	            metadata.subtitle = item.author;
+            // Add metadata
+            var metadata = new 
+               cast.framework.messages.GenericMediaMetadata();
+            metadata.title = item.title;
+            metadata.subtitle = item.author;
 
-	            resolve(request);
-	          }
+            // Resolve request
+            resolve(request);
+          }
         });
       });
     });
-
-let touchControls = cast.framework.ui.Controls.getInstance();
-// Clear default buttons
-touchControls.clearDefaultSlotAssignments();
-touchControls.assignButton(
-    cast.framework.ui.ControlsSlot.SLOT_1,
-    cast.framework.ui.ControlsButton.SEEK_BACKWARD_10
-);
-
-context.start({ touchScreenOptimizedApp: true });
